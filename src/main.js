6b91c726844f24115062a2d5cd5a31802c3e6540
my9074 <313231068@qq.com>
2018-10-30 16:23:03 +0800

    Remove console log

--- src/main.js
+++ src/main.js
@@ -8,8 +8,7 @@ import {
 } from './utils'
 import debounce from 'lodash-es/debounce'
 
-// const DEFAULT_INTERVAL = 1000 * 60 * 2
-const DEFAULT_INTERVAL = 1000 * 30 * 1
+const DEFAULT_INTERVAL = 1000 * 60 * 2
 let isActionFlag = true
 
 function bootstrap () {
@@ -26,23 +25,19 @@ function addActionListener () {
 }
 
 function assignActionFlag () {
-  console.log('user has been click...')
   isActionFlag = true
 }
 
 function healthCheck () {
   if (isTicketSubAccount()) {
-    console.log('fetch backend to validateSession...')
     validateSession(isActionFlag)
       .then(data => {
         if (data.expired) {
-          console.log('expired!!!, should be logout...')
           const companyID = getCompanyID()
           logout().then(data => {
             redirectUrl(companyID)
           })
         }
-        console.log('session is not expired...')
       })
       .catch(err => {
         console.log(err)
